2b8b41d93dc990553ae0d18d4813c4a4ad537562
joshuathomas22 <115492240+joshuathomas22@users.noreply.github.com>
2023-04-21 20:55:06 +0100

    Adds comments to the javascript to explain timeline

--- script.js
+++ script.js
@@ -10,56 +10,59 @@ document.addEventListener('DOMContentLoaded', function(event) {
 })
 
  
-"use strict";
-
+// Code for the timeline animation
 function qs(selector, all = false) {
-  return all ? document.querySelectorAll(selector) : document.querySelector(selector);
-}
-
-const sections = qs('.section', true);
-const timeline = qs('.timeline');
-const line = qs('.line');
-line.style.bottom = `calc(100% - 20px)`;
-let prevScrollY = window.scrollY;
-let up, down;
-let full = false;
-let set = 0;
-const targetY = window.innerHeight * .8;
-
-function scrollHandler(e) {
-  const {
-    scrollY
-  } = window;
-  up = scrollY < prevScrollY;
-  down = !up;
-  const timelineRect = timeline.getBoundingClientRect();
-  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
-
-  const dist = targetY - timelineRect.top;
-  console.log(dist);
-
-  if (down && !full) {
-    set = Math.max(set, dist);
-    line.style.bottom = `calc(100% - ${set}px)`;
+    return all ? document.querySelectorAll(selector) : document.querySelector(selector);
   }
-
-  if (dist > timeline.offsetHeight + 50 && !full) {
-    full = true;
-    line.style.bottom = `-50px`;
-  }
-
-  sections.forEach(item => {
-    // console.log(item);
-    const rect = item.getBoundingClientRect(); //     console.log(rect);
-
-    if (rect.top + item.offsetHeight / 5 < targetY) {
-      item.classList.add('show-me');
+  
+  // Select all elements with class "section" and the timeline element
+  const sections = qs('.section', true); // Select all elements with class "section"
+  const timeline = qs('.timeline'); // Select the timeline element
+  const line = qs('.line'); // Select the line element
+  line.style.bottom = `calc(100% - 20px)`; // Set the line position
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * .8;
+  
+  // Scroll handler function
+  function scrollHandler(e) {
+    const { scrollY } = window;
+    up = scrollY < prevScrollY;
+    down = !up;
+    const timelineRect = timeline.getBoundingClientRect();
+    const lineRect = line.getBoundingClientRect();
+    const dist = targetY - timelineRect.top;
+    
+    if (down && !full) {
+      set = Math.max(set, dist);
+      line.style.bottom = `calc(100% - ${set}px)`;
     }
-  }); // console.log(up, down);
-
-  prevScrollY = window.scrollY;
-}
-
-scrollHandler();
-line.style.display = 'block';
-window.addEventListener('scroll', scrollHandler);
+  
+    if (dist > timeline.offsetHeight + 50 && !full) {
+      full = true;
+      line.style.bottom = `-50px`;
+    }
+  
+    // Loop through all "section" elements and add a class if they're in view
+    sections.forEach(item => {
+      const rect = item.getBoundingClientRect();
+      
+      if (rect.top + item.offsetHeight / 5 < targetY) {
+        item.classList.add('show-me');
+      } else {
+        item.classList.remove('show-me'); // Hide the element if it's not in view
+      }
+    });
+  
+    prevScrollY = window.scrollY;
+  }
+  
+  // Call the scroll handler function once to initialize the timeline
+  scrollHandler();
+  
+  // Set the line to be visible and add the scroll event listener
+  line.style.display = 'block';
+  window.addEventListener('scroll', scrollHandler);
+  
