1fb57d76a62ef88b60b8bb40a251f90d94f949ad
joshuathomas22 <115492240+joshuathomas22@users.noreply.github.com>
2023-05-25 11:55:03 +0100

    Disables controls for the Google map

--- map.js
+++ map.js
@@ -2,15 +2,15 @@ function initMap() {
     var mapProp = {
         center: new google.maps.LatLng(49.098471978152936, -122.7062599561647),
         zoom: 19,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        mapOptions: { disableDefaultUI:true }
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        disableDefaultUI: true
     };
 
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     
 
     new google.maps.Marker({
-        position: { lat: 49.098471978152936, lng: -122.7062599561647 },
+        position: { lat: 49.09857852320189, lng: -122.7062599561647 },
         map,
         animation:google.maps.Animation.BOUNCE
     });
