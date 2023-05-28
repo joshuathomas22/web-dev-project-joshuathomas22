7ce2507cac0aae57e0b7f89112bea10f8c4749fa
joshuathomas22 <115492240+joshuathomas22@users.noreply.github.com>
2023-05-29 00:33:32 +0100

    Adds more comments

--- Javascript/jokes.js
+++ Javascript/jokes.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const getJoke = () => {
-        fetch('https://api.arwenbot.xyz/jokes')
-            .then(response => response.json())
+document.addEventListener('DOMContentLoaded', function () {     // Wait for the DOM to load
+    const getJoke = () => {    // Function to get a joke
+        fetch('https://api.arwenbot.xyz/jokes')  // Fetch a joke from the API
+            .then(response => response.json())  // Convert the response to JSON
             .then(data => {
-                const text = data.data.joke;
-                const textContainer = document.getElementById('jokes');
-                textContainer.innerHTML = text;
+                const text = data.data.joke;    // Get the joke text
+                const textContainer = document.getElementById('jokes'); // Get the element with the ID "jokes"
+                textContainer.innerHTML = text; // Set the text of the element to the joke text
             })
             .catch(error => {
-                console.error('Error:', error);
+                console.error('Error:', error); // Log the error
             });
     }
-    getJoke();
-    const jokeButton = document.getElementById('jokeButton');
+    getJoke();  // Call the function to get a joke
+    const jokeButton = document.getElementById('jokeButton');   // Get the element with the ID "jokeButton"
 
     jokeButton.addEventListener('click', function () {
-        getJoke();
+        getJoke();  // Call the function to get a joke
     })
 })
